f0dcfc8cb2b6b0f2eaace501b1a614fe86acd028
crazydj8 <akshataryan8@gmail.com>
2023-03-09 21:32:12 +0530

    still incomplete

--- Research/Python/rms_error_calculation.py
+++ Research/Python/rms_error_calculation.py
@@ -2,13 +2,10 @@
 import numpy as np
 
 def rms_error_calculation(y, q, tls):
-    n=0
     rms = 0
     tot=0
     for n in range(0, q):
         tot = tot+y[n][5]**2
-    n=0
-    for n in range(0, q):
         rms = rms + ((tls[0][n]-y[n][5])^2/tot)**0.5
 
 #still incomplete

0960e8e766719c3ef8dae39dee14fb7672d9c32e
crazydj8 <akshataryan8@gmail.com>
2023-03-09 21:02:39 +0530

    updated redmat.py

--- Research/Python/Redmat.py
+++ Research/Python/Redmat.py
@@ -4,29 +4,21 @@ import numpy as np
 # Part 1 - U^2 values input - input from low to high energy
 def Redmat():
     x=np.array([]) # U(n)^2 matrix
-    xa = [] # Elements in row 'a'
-    q=0 # number of transitions
-    prompt = 'number of transitions '
-    q = int(input(prompt))
+    q = int(input('number of transitions:'))
 
     for a in range(0, q):
-        prompt = 'U2 '
-        xa.append(float(input(prompt)))
-        prompt = 'U4 '
-        xa.append(float(input(prompt)))
-        prompt = 'U6 '
-        xa.append(float(input(prompt)))
-        x=np.vstack([xa])
-    x = np.reshape(x, (q, 3))
+        x[a][0] = float(input("U2:"))
+        x[a][1] = float(input("U4:"))
+        x[a][2] = float(input("U6:"))
 
     # Part 2- transpose matrix x
-    xt=x.transpose()
+    xt = x.transpose()
 
     # Part 3 - transpose * original
     p=np.dot(xt, x) #product xt * x
 
     # Part 4 - inverse of p
-    ip=np.linalg.inv(p)
+    ip = np.linalg.inv(p)
 
     # Part 5 - ip * xt
     f = np.dot(ip, xt)

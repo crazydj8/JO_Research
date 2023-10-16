663791cd65fa61aaa13659c2a498e64cca051f3c
crazydj8 <akshataryan8@gmail.com>
2023-10-16 17:02:11 +0530

    cleanup and renamed

--- Research/current_build/main.py
+++ Research/current_build/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import stheor, sobs
+import stheor, sobs, rms
 
 df = pd.read_excel("data2.xlsx")
 
@@ -8,5 +8,6 @@ df = stheor.find_stheor(df)
 df = sobs.find_sobs(df)
 
 print(df)
+rms.find_rms(df)
 
 #print(rms.find_rms(df))
--- Research/current_build/sobs.py
+++ Research/current_build/sobs.py
@@ -11,5 +11,5 @@ def find_sobs(df):
     mul_const = df["RI*(3/RI^2+2)^2"]
 
     T = ((8 * np.pi ** 3  * e ** 2 * center * rho) / (3 * light_speed * h * val_2jplus1)) * mul_const
-    df["Sobs"] = ((1 / T) * df["Integrated Area"]) / (10 ** -18)
+    df["Sobs"] = ((1 / T) * df["Integrated Area"]) / (10 ** -20)
     return(df)
